fdaef0a7d5bde775eb7c541e6e70c4b220fa5833
Mikko Kivistö <mikkosakarikivisto@gmail.com>
2021-01-20 13:36:44 +0200

    new backend build

--- backend/build/db.js
+++ backend/build/db.js
@@ -6,11 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Pool = require("pg").Pool;
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-var pool = new Pool({
+var devConfig = {
     user: process.env.DBUSER,
     password: process.env.DBPASS,
     host: process.env.DBHOST,
     port: process.env.DBPORT,
     database: process.env.DBDB,
-});
+};
+var proConfig = {
+    connectionString: process.env.DATABASE_URL
+};
+var pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 exports.default = pool;
