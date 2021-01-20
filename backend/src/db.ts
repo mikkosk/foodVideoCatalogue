5586bf78ee9852603035d8688b78cb91940be56d
Mikko Kivistö <mikkosakarikivisto@gmail.com>
2021-01-20 13:34:26 +0200

    database for deploy

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,12 +2,18 @@ const Pool = require("pg").Pool;
 import dotenv from 'dotenv';
 dotenv.config()
 
-const pool = new Pool({
+const devConfig = {
     user: process.env.DBUSER,
     password: process.env.DBPASS,
     host: process.env.DBHOST,
     port: process.env.DBPORT,
     database: process.env.DBDB,
-})
+}
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
 
 export default pool;
