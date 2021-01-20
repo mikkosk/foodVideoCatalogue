68e0893f5959392a67d9c8f5e2007cb7b24ec4eb
Mikko Kivistö <mikkosakarikivisto@gmail.com>
2021-01-20 12:36:17 +0200

    new build

--- backend/build/app.js
+++ backend/build/app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
+var path_1 = __importDefault(require("path"));
 var videoRouter_1 = __importDefault(require("./routers/videoRouter"));
 var produceRouter_1 = __importDefault(require("./routers/produceRouter"));
 var ingredientRouter_1 = __importDefault(require("./routers/ingredientRouter"));
@@ -13,7 +14,15 @@ var loginRouter_1 = __importDefault(require("./routers/loginRouter"));
 var app = express_1.default();
 app.use(express_1.default.json());
 app.use(cors_1.default());
-app.use(express_1.default.static('buildFrontend'));
+console.log(path_1.default.join(__dirname, '../buildFrontend/build'));
+app.use(express_1.default.static(path_1.default.join(__dirname, '../buildFrontend/build')));
+app.get('/*', function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, '../buildFrontend/build/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
 app.use('/api/user', userRouter_1.default);
 app.use('/api/videos', videoRouter_1.default);
 app.use('/api/ingredients', ingredientRouter_1.default);
